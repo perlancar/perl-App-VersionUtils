7da035a9e77186747235f9b038f0a222e03a572c
perlancar (@pc-home) <perlancar@gmail.com>
2016-07-25 15:13:19 +0700

    Initial commit

---
+++ lib/App/VersionUtils.pm
@@ -0,0 +1,16 @@
+package App::VersionUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Command-line utilities related to version number
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+version number:
+
+#INSERT_EXECS_LIST
+
+=cut
